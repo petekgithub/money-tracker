c36151e6ba61b12647a7f78a725e5517c43d9f86
Petech <petekshamitbeyli@gmail.com>
2023-04-20 00:30:01 +0300

    dispatching-a-login-action

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import { projectAuth } from "../firebase/config";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 export const useSignup = () => {
   const [error, setError] = useState(null);
   const [isPending, setIsPending] = useState(false); // for loading state
+  const { dispatch } = useAuthContext();
 
   // this three fields are coming from the form what  is users entered
   const signup = async (email, password, displayName) => {
@@ -16,12 +18,16 @@ export const useSignup = () => {
         email,
         password
       );
-      console.log(res.user);
+
       if (!res) {
         throw new Error("Could not complete signup");
       }
       // add display name to user
       await res.user.updateProfile({ displayName });
+
+      // dispatch login action
+      dispatch({ type: "LOGIN", payload: res.user });
+
       setIsPending(false);
       setError(null);
     } catch (err) {
@@ -31,5 +37,5 @@ export const useSignup = () => {
     }
   };
 
-  return { error, isPending, signup };
+  return { signup, error, isPending };
 };
